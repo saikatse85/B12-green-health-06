5cc37d6404ad8ca9ae918e8b23c6ced5d9ef5a48
saikatse85 <saikatse@gmail.com>
2025-09-07 22:35:24 +0600

    added alert massage

--- script/index.js
+++ script/index.js
@@ -40,6 +40,29 @@ const loadPlantCategory = (categoryName) => {
         .catch(err => console.log(err));
 }
 
+const showPlantDetails=async(id)=>{
+    const url=`https://openapi.programming-hero.com/api/plant/${id}`
+    const response=await fetch(url);
+    const data =await response.json();
+    displayPlantDetails(data.plants); 
+}
+
+const displayPlantDetails=(plants)=>{
+    console.log(plants);
+    const plantsDetails=document.getElementById('plant-details-container')
+    plantsDetails.innerHTML=`
+        <div class="p-2 space-x-3">
+            <h2 class="font-bold text-2xl">${plants.name}</h2>
+            <img class="rounded-md h-80 w-full my-2" src="${plants.image}" alt="">
+            <p><span class="font-bold">Category:</span>${plants.category}</p>
+            <p class="my-2"><span class="font-bold">Price:</span><i class="fa-solid fa-bangladeshi-taka-sign"></i>${plants.price}</p>
+            <p><span class="font-bold">Description:</span>${plants.description}</p>
+        </div>
+    `
+    document.getElementById('my_modal_5').showModal()
+}
+
+
 // Display Plants Container
 const displayPlants = (plants) => {
     const plantsContainer = document.getElementById("plants-container");
@@ -53,11 +76,11 @@ const displayPlants = (plants) => {
                     <img class="rounded-t-lg h-80 w-full" src="${plant.image}"alt="Plant"/>
                 </figure>
                 <div class="card-body">
-                        <h2 onclick="my_modal_5.showModal()" class="card-title cursor-pointer">${plant.name}</h2>
+                        <h2 onclick="showPlantDetails(${plant.id})" class="card-title cursor-pointer">${plant.name}</h2>
                         <p>${plant.description}</p>
                     <div class="flex justify-between items-center">
-                        <p class="bg-green-100 p-2 rounded-2xl text-green-600">${plant.category}</p>
-                        <p><i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}</p>
+                        <span class="bg-green-100 p-2 rounded-2xl text-green-600">${plant.category}</span>
+                        <span><i class="fa-solid fa-bangladeshi-taka-sign"></i>${plant.price}</span>
                     </div>
                         <button id="add-to-cart" class="btn bg-green-600 text-white w-full rounded-3xl">Add to Cart</button>
                 </div>
@@ -75,6 +98,8 @@ const cartContainer=document.getElementById('cartContainer')
 const addToCart=document.getElementById("plants-container");
 addToCart.addEventListener('click', event=>{
     if(event.target.id === 'add-to-cart'){
+        const alertTitle=event.target.parentNode.children[0].innerText
+        alert(`${alertTitle} has been added to the cart`);
          handleCartItem(event)
     }
 })
